6127cbea538377515d451e2774f3e67120504867
Jacob Gardner <jacob.v.gardner@gmail.com>
2017-11-20 22:11:02 -0600

    Relatively working

--- plugins/paginate/plugin.js
+++ plugins/paginate/plugin.js
@@ -22,29 +22,10 @@
         // icons: 'pagebreak,pagebreak-rtl', // %REMOVE_LINE_CORE%
         hidpi: true, // %REMOVE_LINE_CORE%
         onLoad: function() {
-            // console.log(editor.document);
-            // var cssStyles = (
-            // 		'background:url(' + CKEDITOR.getUrl( this.path + 'images/pagebreak.gif' ) + ') no-repeat center center;' +
-            // 		'clear:both;' +
-            // 		'width:100%;' +
-            // 		'border-top:#999 1px dotted;' +
-            // 		'border-bottom:#999 1px dotted;' +
-            // 		'padding:0;' +
-            // 		'height:7px;' +
-            // 		'cursor:default;'
-            // 	).replace( /;/g, ' !important;' ); // Increase specificity to override other styles, e.g. block outline.
-
-            // // Add the style that renders our placeholder.
-            // CKEDITOR.addCss( 'div.cke_pagebreak{' + cssStyles + '}' );
         },
 
         init: function(editor) {
             editor.once('instanceReady', () => {
-                // editor.document.$;
-                console.log('Document:', editor.document.$);
-                // editor.document.$.body.style.display = 'none';
-                console.log('Document:');
-
                 var src =
                     'document.open();' +
                     // In IE, the document domain must be set any time we call document.open().
@@ -77,12 +58,7 @@
                 var contentSpace = editor.ui.space('contents');
                 document.getElementById('preview').appendChild(iframe.$);
 
-                const editable = editor.editable();
-                // console.log(editable);
-                console.log(editor.document);
-                console.log(editor.document.$.documentElement.innerHTML);
-                console.log(iframe.$.contentWindow);
-                // console.log("HTML:", editor.document);
+				const editable = editor.editable();
 
                 iframe.$.contentWindow.document.documentElement.innerHTML =
                     editor.document.$.documentElement.innerHTML;
@@ -91,32 +67,19 @@
                     const html = editable.getHtml();
 
                     iframe.$.contentWindow.document.body.innerHTML = html;
-                    // console.log(iframe.$.contentWindow.document.body);
-
-                    // console.log(html);
                 });
 
-                // observer.observe(editable.$, {
-
-                // });
-
                 editable.$.addEventListener('input', htmlModified);
-                // evt => {
-                //     const html = editable.getHtml();
-
-                //     iframe.$.contentWindow.document.body.innerHTML = html;
-                //     console.log(iframe.$.contentWindow.document.body);
-
-                //     console.log(html);
-                // });
 
                 const range = document.createRange();
-				const pageHeight = 170;
+				const pageHeight = 50;
 
 				function findSplitElement(root, breakPoint, clientTop, path) {
 					if (!root.childNodes.length) {
 						return root;
 					}
+
+					root.normalize();
 
 					// console.log(Array.from(root.childNodes).map(n => {range.selectNodeContents(n); return [range.getBoundingClientRect().top - clientTop, range.getBoundingClientRect().bottom - clientTop];}).map(r => `[${r[0]}->${r[1]}]`).join(', '))
 					// [3->20], [43->60], [83->100], [123->140], [163->180], [203->220], [243->260], [283->300], [323->340]
@@ -131,6 +94,7 @@
 						range.selectNodeContents(current);
 
 						const rect = range.getBoundingClientRect();
+						console.log('Ranges:', rect.top, clientTop);
 
 						if (breakPoint <= rect.top - clientTop) {
 							if (upperBound - lowerBound === 1) {
@@ -167,18 +131,19 @@
 						range.setStart(element, pivot);
 						range.setEnd(element, pivot);
 
-						console.log(`Range: [${lowerBound}, ${upperBound}]`)
+						// console.log(`Range: [${lowerBound}, ${upperBound}]`)
 
 						const rect = range.getBoundingClientRect();
+						// console.log(rect, rect.top, clientTop)
 
 						if (breakPoint <= rect.top - clientTop) {
-							console.log(`<-] ${breakPoint} <= ${rect.top - clientTop}`);
+							// console.log(`<-] ${breakPoint} <= ${rect.top - clientTop}`);
 							if (upperBound - lowerBound === 1) {
 								return pivot;
 							}
 							upperBound = pivot;
 						} else {
-							console.log(`[-> ${breakPoint} > ${rect.top - clientTop}`);
+							// console.log(`[-> ${breakPoint} > ${rect.top - clientTop}`);
 							if (upperBound - lowerBound === 1) {
 								return pivot + 1;
 							}
@@ -218,7 +183,18 @@
 							throw new Error('Probably in an infinite loop.')
 						}
 
-						splitElements.push([path, pageStart])
+						if (element.nodeType === Node.TEXT_NODE) {
+							const splitOffset = findTextNodeSplit(element, pageStart, clientPageTop);
+							range.setStart(element, splitOffset);
+							range.setEnd(element, splitOffset);
+
+						} else {
+							console.log(element);
+							throw new Error('Not yet implemented.');
+
+						}
+
+						splitElements.push([path, pageStart, element])
 					}
 
 					const newBody = iframe.$.contentWindow.document.body;
@@ -235,14 +211,15 @@
 						}
 					}
 
-					for (const [path, splitY] of splitElements) {
+					for (const [path, splitY, originalElement] of splitElements.reverse()) {
 						const element = getNode(newBody, path);
 
 						console.log(element, newBody, path);
 
 						if (element.nodeType === Node.TEXT_NODE) {
-							const splitOffset = findTextNodeSplit(element, splitY, clientPageTop);
-							console.log('Split Offset:', splitOffset);
+							// console.log('Client TOP:', clientPageTop);
+							const splitOffset = findTextNodeSplit(originalElement, splitY, clientPageTop);
+							// console.log('Split Offset:', splitOffset);
 
 							if (splitOffset === 0) {
 								element.parentNode.insertBefore(document.createElement('hr'), element);
@@ -266,117 +243,11 @@
 							throw new Error("Unsupported node type.")
 						}
 						console.log(element.nodeType);
-
-						// let parent = element;
-
-
 					}
-
-                    // for (let i = 1; i < pageCount; i += 1) {
-                    // 	const pageStart = i * pageHeight;
-
-                    // 	buildOffsetPath()
-
-                    // }
-
-                    // console.log('Full Height:', fullHeight);
-                    // console.log();
-
-                    // children[0]
-
-                    // console.log(children)
-                    // console.log();
-
-                    // console.log(editor.document.$.body);
-
-                    // iframe.$.contentWindow.document.body.innerHTML =
-                    //     html.slice(0, 100) + '<hr />' + html.slice(100);
                 }
-
-                // contentSpace.append(iframe);
             });
-            // console.log(editor.element.$);
-            // editor.document.$.style.display = 'none';
-            // if ( editor.blockless )
-            // 	return;
-
-            // // Register the command.
-            // editor.addCommand( 'pagebreak', CKEDITOR.plugins.pagebreakCmd );
-
-            // // Register the toolbar button.
-            // editor.ui.addButton && editor.ui.addButton( 'PageBreak', {
-            // 	label: editor.lang.pagebreak.toolbar,
-            // 	command: 'pagebreak',
-            // 	toolbar: 'insert,70'
-            // } );
-
-            // // Webkit based browsers needs help to select the page-break.
-            // CKEDITOR.env.webkit && editor.on( 'contentDom', function() {
-            // 	editor.document.on( 'click', function( evt ) {
-            // 		var target = evt.data.getTarget();
-            // 		if ( target.is( 'div' ) && target.hasClass( 'cke_pagebreak' ) )
-            // 			editor.getSelection().selectElement( target );
-            // 	} );
-            // } );
         },
         afterInit: function(editor) {
-            // console.log(editor.document);
-            // // Register a filter to displaying placeholders after mode change.
-            // var dataProcessor = editor.dataProcessor,
-            // 	dataFilter = dataProcessor && dataProcessor.dataFilter,
-            // 	htmlFilter = dataProcessor && dataProcessor.htmlFilter,
-            // 	styleRegex = /page-break-after\s*:\s*always/i,
-            // 	childStyleRegex = /display\s*:\s*none/i;
-
-            // function upcastPageBreak( element ) {
-            // 	CKEDITOR.tools.extend( element.attributes, attributesSet( editor.lang.pagebreak.alt ), true );
-
-            // 	element.children.length = 0;
-            // }
-
-            // if ( htmlFilter ) {
-            // 	htmlFilter.addRules( {
-            // 		attributes: {
-            // 			'class': function( value, element ) {
-            // 				var className = value.replace( 'cke_pagebreak', '' );
-            // 				if ( className != value ) {
-            // 					var span = CKEDITOR.htmlParser.fragment.fromHtml( '<span style="display: none;">&nbsp;</span>' ).children[ 0 ];
-            // 					element.children.length = 0;
-            // 					element.add( span );
-            // 					var attrs = element.attributes;
-            // 					delete attrs[ 'aria-label' ];
-            // 					delete attrs.contenteditable;
-            // 					delete attrs.title;
-            // 				}
-            // 				return className;
-            // 			}
-            // 		}
-            // 	}, { applyToAll: true, priority: 5 } );
-            // }
-
-            // if ( dataFilter ) {
-            // 	dataFilter.addRules( {
-            // 		elements: {
-            // 			div: function( element ) {
-            // 				// The "internal form" of a pagebreak is pasted from clipboard.
-            // 				// ACF may have distorted the HTML because "internal form" is
-            // 				// different than "data form". Make sure that element remains valid
-            // 				// by re-upcasting it (https://dev.ckeditor.com/ticket/11133).
-            // 				if ( element.attributes[ 'data-cke-pagebreak' ] )
-            // 					upcastPageBreak( element );
-
-            // 				// Check for "data form" of the pagebreak. If both element and
-            // 				// descendants match, convert them to internal form.
-            // 				else if ( styleRegex.test( element.attributes.style ) ) {
-            // 					var child = element.children[ 0 ];
-
-            // 					if ( child && child.name == 'span' && childStyleRegex.test( child.attributes.style ) )
-            // 						upcastPageBreak( element );
-            // 				}
-            // 			}
-            // 		}
-            // 	} );
-            // }
         }
     });
 })();
